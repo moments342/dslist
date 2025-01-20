e668b1959895605036dfbfe3ad492e888349455b
podei <gabriellemosfran@gmail.com>
2025-01-20 16:33:09 -0400

    Project Created

---
+++ src/test/java/com/moments/DSList/DsListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moments.DSList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
