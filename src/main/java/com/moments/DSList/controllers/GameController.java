50f8f6b9eb6fbc441554d02dbd52b51130428040
podei <gabriellemosfran@gmail.com>
2025-01-21 18:36:53 -0400

    Get game by id

--- src/main/java/com/moments/DSList/controllers/GameController.java
+++ src/main/java/com/moments/DSList/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.moments.DSList.controllers;
 
+import com.moments.DSList.DTO.GameDTO;
 import com.moments.DSList.DTO.GameMinDTO;
 import com.moments.DSList.entities.Game;
 import com.moments.DSList.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,12 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll() {
